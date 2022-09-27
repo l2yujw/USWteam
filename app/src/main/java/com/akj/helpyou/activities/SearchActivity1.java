a70d74225338006673987b879373051438fac104
이호진 <leehojin0207@naver.com>
2022-09-27 12:28:34 +0900

    t1

--- app/src/main/java/com/akj/helpyou/activities/SearchActivity1.java
+++ app/src/main/java/com/akj/helpyou/activities/SearchActivity1.java
@@ -1,22 +1,95 @@
 package com.akj.helpyou.activities;
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
 
-import androidx.fragment.app.Fragment;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.FragmentActivity;
 
 import com.akj.helpyou.R;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 // E/RecyclerView: No adapter attached; skipping layout -> class : searchaddadapter
-public class SearchActivity1 extends Activity {
+public class SearchActivity1 extends AppCompatActivity implements OnMapReadyCallback, GoogleMap.OnMapClickListener, GoogleMap.OnMarkerDragListener {
 
+    private GoogleMap mMap;
+    private LocationManager manager;
+    private String TAG = "SearchActivity1";
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search1);
 
 
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.S1map);
+        mapFragment.getMapAsync(this);
+
+        manager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
     }
 
 
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            return;
+        }
+        Location location = manager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+
+        LatLng loc = new LatLng(location.getLatitude(), location.getLongitude());
+        //LatLng loc = new LatLng(37.566, 126.9775);
+        Log.d(TAG, "onMapReady: "+location.getLatitude()+" / "+location.getLongitude());
+        mMap = googleMap;
+
+        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(loc, 15);
+        mMap.moveCamera(cameraUpdate);                 // 초기 위치
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(15));                         // 줌의 정도
+        googleMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+
+
+
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+        super.onPointerCaptureChanged(hasCapture);
+    }
+
+    @Override
+    public void onMapClick(@NonNull LatLng latLng) {
+
+    }
+
+    @Override
+    public void onMarkerDrag(@NonNull Marker marker) {
+
+    }
+
+    @Override
+    public void onMarkerDragEnd(@NonNull Marker marker) {
+
+    }
+
+    @Override
+    public void onMarkerDragStart(@NonNull Marker marker) {
+
+    }
 }
