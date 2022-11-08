0fc679083cf7409657d446f5c6b5b73c13280e6a
이호진 <leehojin0207@naver.com>
2022-11-08 13:11:16 +0900

    어플 켜면 SplashActivity -> MainActivity로 이동 SplashActivity에서 LocationPermission검사

--- app/src/main/java/com/akj/helpyou/activities/MainActivity.java
+++ app/src/main/java/com/akj/helpyou/activities/MainActivity.java
@@ -20,6 +20,8 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
@@ -73,17 +75,6 @@ public class MainActivity extends AppCompatActivity implements MapView.CurrentLo
         BusThread.start();
 
         lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-
-            return;
-        }
-        loc_Current = lm.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
-        // 맵뷰 생성
-        mapView = new MapView(this);
-        mapViewContainer = (ViewGroup) findViewById(R.id.map_view);
-        mapViewContainer.addView(mapView);
-        mapView.setMapViewEventListener(this);
-        mapView.setPOIItemEventListener(this);
 
         //플로팅 액션 버튼 생성
         FloatingActionButton fab_btn1 = (FloatingActionButton) findViewById(R.id.fab_location);
@@ -97,11 +88,6 @@ public class MainActivity extends AppCompatActivity implements MapView.CurrentLo
                 fab_location_count++;
                 if (fab_location_count % 2 == 1) {
                     mapView.setCurrentLocationTrackingMode(MapView.CurrentLocationTrackingMode.TrackingModeOnWithoutHeading);
-                    if (!checkLocationServicesStatus()) {
-                        showDialogForLocationServiceSetting();
-                    } else {
-                        checkRunTimePermission();
-                    }
                 } else {
                     mapView.setCurrentLocationTrackingMode(MapView.CurrentLocationTrackingMode.TrackingModeOff);
                 }
@@ -117,10 +103,20 @@ public class MainActivity extends AppCompatActivity implements MapView.CurrentLo
             public void onClick(View view) {
                 //api에서 충전소 리스트 불러와서 가까운 순서로 정렬
                 findelectricstation();
-
             }
         });
 
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(this, "권한 없음", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        loc_Current = lm.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+        // 맵뷰 생성
+        mapView = new MapView(this);
+        mapViewContainer = (ViewGroup) findViewById(R.id.map_view);
+        mapViewContainer.addView(mapView);
+        mapView.setMapViewEventListener(this);
+        mapView.setPOIItemEventListener(this);
 
         // 툴바 생성
         toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -173,6 +169,7 @@ public class MainActivity extends AppCompatActivity implements MapView.CurrentLo
             }
         });
 
+
     } // onCreate 마지막 줄
 
 
@@ -194,10 +191,10 @@ public class MainActivity extends AppCompatActivity implements MapView.CurrentLo
         Thread thread = new Thread() {
             @Override
             public void run() {
-                Log.d(LOG_TAG, "run: "+finalLatitude+" / "+finalLongitude);
-                List<Chargestation> list =  db.ChargestationDao().FindStationin1(finalLatitude, finalLongitude);//근처 리스트 리턴
-                Log.d(LOG_TAG, "run: "+list);
-                for(int i=0;i<list.size();i++){
+                Log.d(LOG_TAG, "run: " + finalLatitude + " / " + finalLongitude);
+                List<Chargestation> list = db.ChargestationDao().FindStationin1(finalLatitude, finalLongitude);//근처 리스트 리턴
+                Log.d(LOG_TAG, "run: " + list);
+                for (int i = 0; i < list.size(); i++) {
                     MapPoint mappoint = MapPoint.mapPointWithGeoCoord(list.get(i).getLat(),
                             list.get(i).getLng());
 
@@ -215,7 +212,7 @@ public class MainActivity extends AppCompatActivity implements MapView.CurrentLo
         thread.start();
 
 
-        mapView.setMapCenterPointAndZoomLevel(centerpoint,4,true);
+        mapView.setMapCenterPointAndZoomLevel(centerpoint, 4, true);
 
 
     }
@@ -223,8 +220,8 @@ public class MainActivity extends AppCompatActivity implements MapView.CurrentLo
     // 메인 화면에서 사이드바의 메뉴 버튼을 클릭했을 때
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()){
-            case android.R.id.home:{ // 왼쪽 상단 버튼 눌렀을 때
+        switch (item.getItemId()) {
+            case android.R.id.home: { // 왼쪽 상단 버튼 눌렀을 때
                 drawerLayout.openDrawer(GravityCompat.START);
                 return true;
             }
@@ -253,6 +250,7 @@ public class MainActivity extends AppCompatActivity implements MapView.CurrentLo
         MapPoint.GeoCoordinate mapPointGeo = currentLocation.getMapPointGeoCoord();
         Log.i(LOG_TAG, String.format("MapView onCurrentLocationUpdate (%f,%f) accuracy (%f)", mapPointGeo.latitude, mapPointGeo.longitude, accuracyInMeters));
     }
+
     @Override
     public void onCurrentLocationDeviceHeadingUpdate(MapView mapView, float v) {
     }
@@ -270,125 +268,10 @@ public class MainActivity extends AppCompatActivity implements MapView.CurrentLo
 //        Toast.makeText(LocationDemoActivity.this, "Reverse Geo-coding : " + result, Toast.LENGTH_SHORT).show();
     }
 
-    // ActivityCompat.requestPermissions를 사용한 퍼미션 요청의 결과를 리턴받는 메소드
-    @Override
-    public void onRequestPermissionsResult(int permsRequestCode,
-                                           @NonNull String[] permissions,
-                                           @NonNull int[] grandResults) {
-
-        super.onRequestPermissionsResult(permsRequestCode, permissions, grandResults);
-        if (permsRequestCode == PERMISSIONS_REQUEST_CODE && grandResults.length == REQUIRED_PERMISSIONS.length) {
-
-            // 요청 코드가 PERMISSIONS_REQUEST_CODE 이고, 요청한 퍼미션 개수만큼 수신되었다면
-            boolean check_result = true;
-
-            // 모든 퍼미션을 허용했는지 체크합니다.
-            for (int result : grandResults) {
-                if (result != PackageManager.PERMISSION_GRANTED) {
-                    check_result = false;
-                    break;
-                }
-            }
-
-            if (check_result) {
-                Log.d("@@@", "start");
-                //위치 값을 가져올 수 있음
-
-            } else {
-                // 거부한 퍼미션이 있다면 앱을 사용할 수 없는 이유를 설명해주고 앱을 종료합니다.2 가지 경우가 있다
-                if (ActivityCompat.shouldShowRequestPermissionRationale(this, REQUIRED_PERMISSIONS[0])) {
-                    Toast.makeText(MainActivity.this, "퍼미션이 거부되었습니다. 앱을 다시 실행하여 퍼미션을 허용해주세요.", Toast.LENGTH_LONG).show();
-                    finish();
-                } else {
-                    Toast.makeText(MainActivity.this, "퍼미션이 거부되었습니다. 설정(앱 정보)에서 퍼미션을 허용해야 합니다. ", Toast.LENGTH_LONG).show();
-                }
-            }
-        }
-    }
-
-    void checkRunTimePermission(){
-
-        //런타임 퍼미션 처리
-        // 1. 위치 퍼미션을 가지고 있는지 체크합니다.
-        int hasFineLocationPermission = ContextCompat.checkSelfPermission(MainActivity.this,
-                Manifest.permission.ACCESS_FINE_LOCATION);
-
-        if (hasFineLocationPermission == PackageManager.PERMISSION_GRANTED ) {
-            // 2. 이미 퍼미션을 가지고 있다면
-            // ( 안드로이드 6.0 이하 버전은 런타임 퍼미션이 필요없기 때문에 이미 허용된 걸로 인식합니다.)
-            // 3.  위치 값을 가져올 수 있음
-
-        } else {  //2. 퍼미션 요청을 허용한 적이 없다면 퍼미션 요청이 필요합니다. 2가지 경우(3-1, 4-1)가 있습니다.
-            // 3-1. 사용자가 퍼미션 거부를 한 적이 있는 경우에는
-            if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this, REQUIRED_PERMISSIONS[0])) {
-                // 3-2. 요청을 진행하기 전에 사용자가에게 퍼미션이 필요한 이유를 설명해줄 필요가 있습니다.
-                Toast.makeText(MainActivity.this, "이 앱을 실행하려면 위치 접근 권한이 필요합니다.", Toast.LENGTH_LONG).show();
-                // 3-3. 사용자게에 퍼미션 요청을 합니다. 요청 결과는 onRequestPermissionResult에서 수신됩니다.
-                ActivityCompat.requestPermissions(MainActivity.this, REQUIRED_PERMISSIONS,
-                        PERMISSIONS_REQUEST_CODE);
-            } else {
-                // 4-1. 사용자가 퍼미션 거부를 한 적이 없는 경우에는 퍼미션 요청을 바로 합니다.
-                // 요청 결과는 onRequestPermissionResult에서 수신됩니다.
-                ActivityCompat.requestPermissions(MainActivity.this, REQUIRED_PERMISSIONS,
-                        PERMISSIONS_REQUEST_CODE);
-            }
-        }
-    }
-
-    //여기부터는 GPS 활성화를 위한 메소드들
-    private void showDialogForLocationServiceSetting() {
-
-        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-        builder.setTitle("위치 서비스 비활성화");
-        builder.setMessage("앱을 사용하기 위해서는 위치 서비스가 필요합니다.\n"
-                + "위치 설정을 수정하시겠습니까?");
-        builder.setCancelable(true);
-        builder.setPositiveButton("설정", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int id) {
-                Intent callGPSSettingIntent
-                        = new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS);
-                startActivityForResult(callGPSSettingIntent, GPS_ENABLE_REQUEST_CODE);
-            }
-        });
-        builder.setNegativeButton("취소", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int id) {
-                dialog.cancel();
-            }
-        });
-        builder.create().show();
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        switch (requestCode) {
-            case GPS_ENABLE_REQUEST_CODE:
-                //사용자가 GPS 활성 시켰는지 검사
-                if (checkLocationServicesStatus()) {
-                    if (checkLocationServicesStatus()) {
-                        Log.d("@@@", "onActivityResult : GPS 활성화 되있음");
-                        checkRunTimePermission();
-                        return;
-                    }
-                }
-                break;
-        }
-    }
-
-    public boolean checkLocationServicesStatus() {
-        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
-
-        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
-                || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
-    }
-
-    public String getCurrentAddress(double latitude, double longitude){
+    public String getCurrentAddress(double latitude, double longitude) {
         Geocoder geocoder = new Geocoder(this, Locale.KOREA);
         List<Address> addresses = null;
-        try{
+        try {
             addresses = geocoder.getFromLocation(
                     latitude,
                     longitude,
@@ -398,8 +281,8 @@ public class MainActivity extends AppCompatActivity implements MapView.CurrentLo
             e.printStackTrace();
         }
 
-        if(addresses == null || addresses.size()==0){
-            Toast.makeText(this,"주소 미발견",Toast.LENGTH_SHORT).show();
+        if (addresses == null || addresses.size() == 0) {
+            Toast.makeText(this, "주소 미발견", Toast.LENGTH_SHORT).show();
             return "주소 미발견";
         }
 
@@ -407,8 +290,8 @@ public class MainActivity extends AppCompatActivity implements MapView.CurrentLo
     }
 
 
-
-    @Override // MapView가 사용가능 한 상태가 되었음을 알려준다. onMapViewInitialized()가 호출된 이후에 MapView 객체가 제공하는 지도 조작 API들을 사용할 수 있다.
+    @Override
+    // MapView가 사용가능 한 상태가 되었음을 알려준다. onMapViewInitialized()가 호출된 이후에 MapView 객체가 제공하는 지도 조작 API들을 사용할 수 있다.
     public void onMapViewInitialized(MapView mapView) {
 
     }
@@ -470,13 +353,13 @@ public class MainActivity extends AppCompatActivity implements MapView.CurrentLo
         Location location = lm.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
         double latitude = location.getLatitude(); // 경도
         double longitude = location.getLongitude(); // 위도
-        String startpoint = getCurrentAddress(latitude,longitude);
+        String startpoint = getCurrentAddress(latitude, longitude);
         startpoint = startpoint.split("대한민국 ")[1];
         String endpoint = mapPOIItem.getItemName();
 
-        Intent intent = new Intent(MainActivity.this,FindRoadActivity.class);
-        intent.putExtra("startPoint",startpoint);
-        intent.putExtra("endPoint",endpoint);
+        Intent intent = new Intent(MainActivity.this, FindRoadActivity.class);
+        intent.putExtra("startPoint", startpoint);
+        intent.putExtra("endPoint", endpoint);
         startActivity(intent);
 
     }
